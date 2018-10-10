6b187dae35ce30c2afe45c2bb8a4a5dcf4881c6c
Joey <joey@lineageos.org>
2018-10-10 08:44:12 +0200

    Revert "Removing support for app prediction from Launcher3"
    This reverts commit b1d222e9a76cc733cead3343a4b7c1e39daa190c.
    
    Change-Id: I5d7ed938c9817527c251a08b57a7f1e6564f3e28

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -67,6 +67,12 @@ public class InvariantDeviceProfile {
     public int numAllAppColumns;
 
     /**
+     * The minimum number of predicted apps in all apps.
+     */
+    //@Deprecated
+    int minAllAppsPredictionColumns;
+
+    /**
      * Number of icons per row and column in the folder.
      */
     public int numFolderRows;
@@ -96,13 +102,13 @@ public class InvariantDeviceProfile {
 
     private InvariantDeviceProfile(InvariantDeviceProfile p) {
         this(p.name, p.minWidthDps, p.minHeightDps, p.numRows, p.numColumns, p.numAllAppColumns,
-                p.numFolderRows, p.numFolderColumns,
+                p.numFolderRows, p.numFolderColumns, p.numHotseatIcons,
                 p.iconSize, p.landscapeIconSize, p.iconTextSize, p.numHotseatIcons,
                 p.defaultLayoutId, p.demoModeLayoutId);
     }
 
     private InvariantDeviceProfile(String n, float w, float h, int r, int c, int nac, int fr, int fc,
-            float is, float lis, float its, int hs, int dlId, int dmlId) {
+                           int maapc, float is, float lis, float its, int hs, int dlId, int dmlId) {
         name = n;
         minWidthDps = w;
         minHeightDps = h;
@@ -111,6 +117,7 @@ public class InvariantDeviceProfile {
         numFolderRows = fr;
         numFolderColumns = fc;
         numAllAppColumns = nac;
+        minAllAppsPredictionColumns = maapc;
         iconSize = is;
         landscapeIconSize = lis;
         iconTextSize = its;
@@ -148,6 +155,7 @@ public class InvariantDeviceProfile {
         demoModeLayoutId = closestProfile.demoModeLayoutId;
         numFolderRows = closestProfile.numFolderRows;
         numFolderColumns = closestProfile.numFolderColumns;
+        minAllAppsPredictionColumns = closestProfile.minAllAppsPredictionColumns;
 
         float iconSizeModifier = Utilities.getIconSizeModifier(context);
         iconSize = interpolatedDeviceProfileOut.iconSize * iconSizeModifier;
@@ -207,6 +215,7 @@ public class InvariantDeviceProfile {
                             numAllAppColumns,
                             a.getInt(R.styleable.InvariantDeviceProfile_numFolderRows, numRows),
                             a.getInt(R.styleable.InvariantDeviceProfile_numFolderColumns, numColumns),
+                            a.getInt(R.styleable.InvariantDeviceProfile_minAllAppsPredictionColumns, numColumns),
                             iconSize,
                             a.getFloat(R.styleable.InvariantDeviceProfile_landscapeIconSize, iconSize),
                             a.getFloat(R.styleable.InvariantDeviceProfile_iconTextSize, 0),
